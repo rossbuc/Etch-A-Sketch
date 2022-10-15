183400fc5bb45c98d324fca834e7959d059abe5a
Ross Buchan <rossbuchan1@googlemail.com>
2022-10-15 10:47:12 +0100

    Generate grid layout and populate with grid cells using js

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.getElementById("container"); 
+
+for (let x = 0; x < 17; x++) {
+    for (let y = 0; y < 17; y++) {
+        var cell = document.createElement("div");
+        cell.setAttribute("grid-column", x);
+        cell.setAttribute("grid-row",  y);
+        cell.setAttribute("class", "grid-cell");
+        grid.appendChild(cell);
+    }
+}
