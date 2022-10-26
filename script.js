56f1e0975cd49c0d437f23e3e78e5cb8f03ed470
Ross Buchan <rossbuchan1@googlemail.com>
2022-10-26 10:06:45 +0100

    Add feature to enable user to set grid size

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const grid = document.getElementById("container"); 
-
-for (let x = 0; x < 17; x++) {
-    for (let y = 0; y < 17; y++) {
-        var cell = document.createElement("div");
-        cell.setAttribute("grid-column", x);
-        cell.setAttribute("grid-row",  y);
-        cell.setAttribute("class", "grid-cell");
-        grid.appendChild(cell);
-    }
+function grdPrompt() {
+    var gridSize = prompt("Please enter the number of grid squares you would like!");
+    gridBuild(gridSize); 
 }
 
-grid.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = "yellow"; 
-});
+function gridBuild(gridSize) {
+    const grid = document.getElementById("container"); 
+    document.getElementById("container").style.gridTemplateRows = `repeat(${gridSize}, 1fr)`; 
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; 
+
+    for (let x = 0; x < gridSize; x++) {
+        for (let y = 0; y < gridSize; y++) {
+            var cell = document.createElement("div");
+            cell.setAttribute("grid-column", x);
+            cell.setAttribute("grid-row",  y);
+            cell.setAttribute("class", "grid-cell");
+            grid.appendChild(cell);
+        }
+    }
+
+    grid.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "yellow"; 
+    });
+}
+
+
+
